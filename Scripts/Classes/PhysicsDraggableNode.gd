8e7f2ffe49a6b1f899faeae33419ad2688465576
Jared <158999104+jazi2018@users.noreply.github.com>
2025-08-26 23:54:22 -0700

    Implement PhysicsDraggableNode
    Now has nodes that can be dragged using physics in a fun and whacky way - can reduce spring constant to make it a little whackier!
    There is a small bug but in a magic setting surely it's fine teehee

--- Scripts/Classes/PhysicsDraggableNode.gd
+++ Scripts/Classes/PhysicsDraggableNode.gd
@@ -2,6 +2,25 @@ class_name PhysicsDraggableNode
 extends RigidBody2D
 
 ## Same as a draggable node- but with physics
-const SPRING_CONSTANT := 500
+const SPRING_CONSTANT := 200
+var has_mouse: bool = false
+var held: bool = false
+var awaiting_release = false
+#func apply_impulse_to_mouse() -> void:
+	#self.apply_central_impulse(SPRING_CONSTANT * get_local_mouse_position() - self.linear_velocity / 4)
 
-# finish this up
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("left_click") and (self.has_mouse or self.awaiting_release):
+		held = true
+		self.apply_central_impulse(SPRING_CONSTANT * get_local_mouse_position() * delta - self.linear_velocity / 4)
+	elif Input.is_action_just_released("left_click"):
+		self.awaiting_release = false
+		#for some reason if the object is perfectly still it just floats there? i'm chill w it but yeah
+
+func _on_mouse_entered() -> void:
+	self.has_mouse = true
+
+func _on_mouse_exited() -> void:
+	self.has_mouse = false
+	if Input.is_action_pressed("left_click"):
+		self.awaiting_release = true
