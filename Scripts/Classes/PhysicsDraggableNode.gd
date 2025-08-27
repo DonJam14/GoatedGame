8648dfa6d21d6920d2fdcdf6306faf311f819732
Jared <158999104+jazi2018@users.noreply.github.com>
2025-08-27 15:00:51 -0700

    Update PhysicsDraggableNode
    Drag stuff using physics

--- Scripts/Classes/PhysicsDraggableNode.gd
+++ Scripts/Classes/PhysicsDraggableNode.gd
@@ -2,20 +2,26 @@ class_name PhysicsDraggableNode
 extends RigidBody2D
 
 ## Same as a draggable node- but with physics
-const SPRING_CONSTANT := 200
+## Requires: 
+## can_sleep = false
+## lock_rotation = true (though this could be changed later if apply impulse function factors in rotation)
+## collision_layer = 2
+## input_pickable = true
+const SPRING_CONSTANT := 250
 var has_mouse: bool = false
-var held: bool = false
+#var held: bool = false
 var awaiting_release = false
-#func apply_impulse_to_mouse() -> void:
-	#self.apply_central_impulse(SPRING_CONSTANT * get_local_mouse_position() - self.linear_velocity / 4)
+
+func apply_impulse_to_mouse(modifier: float) -> void:
+	self.apply_central_impulse(SPRING_CONSTANT * modifier * get_local_mouse_position() - self.linear_velocity / 4)
 
 func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("left_click") and (self.has_mouse or self.awaiting_release):
-		held = true
-		self.apply_central_impulse(SPRING_CONSTANT * get_local_mouse_position() * delta - self.linear_velocity / 4)
+		#held = true
+		apply_impulse_to_mouse(delta)
 	elif Input.is_action_just_released("left_click"):
 		self.awaiting_release = false
-		#for some reason if the object is perfectly still it just floats there? i'm chill w it but yeah
+		#held = false
 
 func _on_mouse_entered() -> void:
 	self.has_mouse = true
